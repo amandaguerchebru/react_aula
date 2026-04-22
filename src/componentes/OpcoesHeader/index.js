d246b300c4392d1f25d0c33544b5aef7b75de971
Amanda Guerche Bruzadin <amanda.bruzadin@aluno.senai.br>
2026-04-22 12:04:33 -0300

    almoço

---
+++ src/componentes/OpcoesHeader/index.js
@@ -0,0 +1,15 @@
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE'];
+// AAAAAAAAAAAARRAY
+
+function OpcoesHeader() {
+    return (
+        <ul className='opcoes'>
+            {/* array function - quando alterar a array, ele já vem pra cá */}
+            {textoOpcoes.map((texto) => (
+                <li className='opcao'><p>{texto}</p></li>
+            ))}
+        </ul>
+    )
+}
+
+export default OpcoesHeader
