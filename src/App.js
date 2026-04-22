1613424ddbd26f7441c4b3b2f14fce98ec3c84c9
Amanda Guerche Bruzadin <amanda.bruzadin@aluno.senai.br>
2026-04-22 15:17:37 -0300

    intervalo - tarde

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 // Estamos fazendo refatoração de código
 import './App.css';
+import IconesHeader from './componentes/IconesHeader';
 import Logo from './componentes/Logo';
 import OpcoesHeader from './componentes/OpcoesHeader';
-import perfil from './imagens/perfil.svg';
-import sacola from './imagens/sacola.svg';
 
-const icones = [perfil, sacola];
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        {/* nome da classe minusculo por ser importado */}
-        {/* segurando o ctrl conseguimos ver a função */}
-        {/* os componentes geram */}
-        <Logo></Logo>
-        <OpcoesHeader></OpcoesHeader>
-        
-      </header>
+      
     </div>
   );
 }
---
+++ src/componentes/Header/index.js
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <header className='App-header'>
+            {/* os componentes geram tags*/}
+            <Logo />
+            <OpcoesHeader />
+            <IconesHeader />
+        </header>
+    )
+}
+
+/* nome da classe minusculo por ser importado */
+/* segurando o ctrl conseguimos ver a função */
