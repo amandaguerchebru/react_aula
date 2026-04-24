352a2aed4d723472c498eb36aa6c165170861aeb
Amanda Guerche Bruzadin <amanda.bruzadin@aluno.senai.br>
2026-04-24 16:40:29 -0300

    finalização dia 24/04
    ainda não acabou

--- src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -1,5 +1,8 @@
-const Livros = [
-    { nome: 'Liderança em Design', id: 1 },
-    { nome: 'Apache Kafka', id: 2 },
-    { nome: 'Cangaceiro em JavaScript', id: 3 },
+import livro from '../../imagens/livro.png';
+import styled from 'styled-components';
+
+export const livros = [
+    { nome: 'Liderança em Design', src:livro, id: 1 },
+    { nome: 'Apache Kafka', src:livro, id: 2},
+    { nome: 'Cangaceiro em JavaScript', src:livro, id: 3},
 ]
